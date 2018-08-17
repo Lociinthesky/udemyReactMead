498f01931b600743595bb60469c882fda45c915e
Lance Tschirhart <lociinthesky@gmail.com>
2018-08-17 09:02:13 -0500

    Everything Working fine once more!

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -1,57 +1,53 @@
 class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    getGreeting() {
-        return `Hey, I'm ${this.name}.`
-    }
-    getDescription() {
-        return `${this.name} is ${this.age} years old.`;
-    }
+  constructor(name = 'Anonymous', age = 0) {
+    this.name = name;
+    this.age = age;
+  }
+  getGreeting() {
+    return `Hi. I am ${this.name}!`;
+  }
+  getDescription() {
+    return `${this.name} is ${this.age} year(s) old.`;
+  }
 }
-
-var me = new Person('Lancelot Tschirhart', 30);
-console.log(me.getGreeting());
-console.log(me.getDescription());
 
 class Student extends Person {
-    constructor(name, age, major) {
-        super(name, age)
-        this.major = major
-    }   
-    hasMajor() {
-        return !!this.major;
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
+  hasMajor() {
+    return !!this.major;
+  }
+  getDescription() {
+    let description = super.getDescription();
+
+    if (this.hasMajor()) {
+      description += ` Their major is ${this.major}.`;
     }
-    getDescription() {
-        let description = super.getDescription();
-        if ( this.hasMajor() ) {
-            description += `Their major is ${this.major}.`;
-        }
-        return description;
-    }
+
+    return description;
+  }
 }
-
-var she = new Student('KTK', 40, 'School of Life');
-console.log(she.getGreeting());
-console.log(she.getDescription());
-console.log(she.hasMajor());
-
 
 class Traveler extends Person {
-    constructor(name, age, homeLocation) {
-        super(name, age);
-        this.homeLocation = homeLocation;
-    }
-    divulgedLocation() {
-        return !!this.homeLocation;
+  constructor(name, age, homeLocation) {
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+  getGreeting() {
+    let greeting = super.getGreeting();
+
+    if (this.homeLocation) {
+      greeting += ` I am visiting from ${this.homeLocation}.`;
     }
 
-    getGreeting() {
-        let greet = super.getGreeting();
-        if (this.divulgedLocation()) {
-            greet += `  I come from ${this.homeLocation}.`
-        }
-        return greet;
-    }
+    return greeting;
+  }
 }
+
+const me = new Traveler('Andrew Mead', 26, 'Philadelphia');
+console.log(me.getGreeting());
+
+const other = new Traveler(undefined, undefined, 'Nowhere');
+console.log(other.getGreeting());
--- indecision-app/webpack.config.js
+++ indecision-app/webpack.config.js
@@ -1,15 +1,20 @@
-const path = require('path')
-
-//two critical pieces of info:  entry -> output
-//console.log(__dirname);
-
-//export object to another file (webpack)
-//node webpack.config.js gives absolute path
+const path = require('path');
 
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    }
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
 };
