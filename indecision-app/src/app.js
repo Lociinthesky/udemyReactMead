97e3b84fb76c69ec8aa4ae1443f5092322aa3adb
Lance Tschirhart <lociinthesky@gmail.com>
2018-08-17 19:07:37 -0500

    back up through webpack with notes

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,5 +1,8 @@
+import { square, add } from './utils.js'
+import { isAdult, canDrink } from './person.js'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
+console.log(square(4));
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
---
+++ indecision-app/src/utils.js
@@ -0,0 +1,10 @@
+console.log('utils.js is running')
+
+const square = x => x * x;
+const add = (a, b) => a + b;
+
+//2 kinds of exports  default exports and named exports.
+
+//named:
+
+export { add, square } 
